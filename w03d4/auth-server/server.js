2606518b6a5d15821c948612a15b13509b312eca
Pedro A. González <pgonzalez@post.com>
2022-11-24 01:39:17 -0500

    Add process env port to test render deployment

--- w03d4/auth-server/server.js
+++ w03d4/auth-server/server.js
@@ -33,7 +33,7 @@ const usersDB = [
 
 // ----------------------- SETUP AND MIDDLEWARES
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(helmet()); // includes security headers (owasp)
 app.use(morgan('dev')); // middleware that logs all the requests
